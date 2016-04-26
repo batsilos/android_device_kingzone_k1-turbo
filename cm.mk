6778e13ed5848efad17213a5c49d5d8b4e1d1f14
Batsilos <batsilos@gmail.com>
2016-04-26 11:50:41 +0300

    Change to G4S

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := K1_turbo
+PRODUCT_RELEASE_NAME := G4s
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/kingzone/k1_turbo/device_k1-turbo.mk)
+$(call inherit-product, device/jiayu/g4s/device_g4s.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k1_turbo
-PRODUCT_NAME := cm_k1_turbo
-PRODUCT_BRAND := KINGZONE
-PRODUCT_MODEL := K1_turbo
-PRODUCT_MANUFACTURER := KINGZONE
+PRODUCT_DEVICE := g4s
+PRODUCT_NAME := cm_g4s
+PRODUCT_BRAND := JIAYU
+PRODUCT_MODEL := G4s
+PRODUCT_MANUFACTURER := JIAYU
